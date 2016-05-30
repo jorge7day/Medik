6c8d0d554798dcb9bb08d44e9f72d6de4c4c8c53
Jorge Otoniel <otto.7day@hotmail.com>
2016-05-29 22:20:14 -0600

    La cita debe ser modificada para obtener correctamente los intervalos de tiempo de las citas

--- php/Cita.php
+++ php/Cita.php
@@ -1,5 +1,7 @@
 <?php
-include 'database.php';
+namespace clases;
+
+use clases\Database;
 /**
  * Description of Cita
  *
@@ -23,6 +25,13 @@ class Cita {
     CONST VISIBLE = 1;
     CONST INVISIBLE = 0;
     
+    //INFORMACIÓN DE CITAS
+//    const tiempo_max = "01:00:00";
+//    const entrada_mañana = "09:00:00";
+//    const salida_mañana = "12:00:00";
+//    const entrada_tarde = "13:00:00";
+//    const salida_tarde = "20:00:00";
+
     //ATRIBUTOS DE UNA CITA
     public $codigo_cita;
     public $codigo_medico = -1;
@@ -33,16 +42,33 @@ class Cita {
     private $activo = self::COL_ACTIVO;
     private $visible = self::VISIBLE;
     
+    //
+    private $intervalo_cita;
+    private $entrada_mañana;
+    private $salida_mañana;
+    private $entrada_tarde;
+    private $salida_tarde;
+    
     /**
      * Para construir una cita, se necesita del código del médico, del código del paciente y del motivo de su cita
      * @param type $codigo_medico
      * @param type $codigo_paciente
      * @param type $motivo
      */
-    function __construct($codigo_medico, $codigo_paciente, $motivo) {
-        $this->codigo_medico = $codigo_medico;
+    function __construct($codigo_paciente, $motivo) {
         $this->codigo_paciente = $codigo_paciente;
         $this->motivo = $motivo;
+        
+        $this->codigo_medico = 1;
+        
+        $this->activo = self::ACTIVO;
+        $this->visible = self::VISIBLE;
+        
+        $this->intervalo_cita = new \DateInterval("30 min");
+        $this->entrada_mañana = date_create("09:00:00");
+        $this->salida_mañana = date_create("12:00:00");
+        $this->entrada_tarde = date_create("13:00:00");
+        $this->salida_tarde = date_create("20:00:00");
     }
     
     function isActivo() {
@@ -100,12 +126,94 @@ class Cita {
     }
     
     /**
+     * 
+     */
+    private function getNextTurno() {
+        //include 'cita.php';
+        $database = new Database();
+        $fecha = null; //Variable que almacenará la fecha y hora de la cita
+        $f_base = null;
+        
+        do {
+            //Bandera que refleja si la hora y fecha están en el rango de trabajo del médico
+            $isReady = true;
+            
+            //Obteniendo el objeto fecha del siguiente día
+            $f_base = date_create();
+            $f_base->add(\DateInterval::createFromDateString("1 day"));
+            
+            //GUardando el String de la fecha
+            $temp = date_format($f_base, "Y-m-d");
+            
+            //Construyendo sentencia para obtener el último turno del siguiente día
+            $sentencia = "select max(" . self::COL_F_CITA . ") from " . Database::TABLA_CITA 
+                    . " where " . self::COL_F_CITA 
+                    . " between '" . $temp . " " . self::entrada_mañana
+                    . "' and '" . $temp . " " . self::salida_tarde . "'";
+
+            //Se ejecuta la sentencia que extrae el último turno
+            $res = $database->request($sentencia);
+            
+            //Ahora que se han obtenido todas las citas para ese día, hay que ver cuál es el turno más tardío
+            if($res == null) {
+                return null;
+            }
+            
+            $row = mysqli_fetch_assoc($res);
+
+            //$last_date = date_create_from_format("Y-m-d H:i:s", $row["max(" . self::COL_F_CITA . ")"]);
+
+            //Buscando la posible hora y fecha de la nueva cita
+            if($fecha == null) {
+                $fecha = date_create_from_format("Y-m-d H:i:s", $row["max(" . self::COL_F_CITA . ")"]);
+            }
+            
+            //Se le suman 30 minutos a la hora, para mover la cita
+            //media hora más tarde
+            $fecha->add(\DateInterval::createFromDateString("30 minute"));
+
+            //Creando horarios de corte del servicio
+            //$entrada_mañana = date_create($temp . " " . self::entrada_mañana);
+            $salida_mañana = date_create($temp . " " . self::salida_mañana);
+            $entrada_tarde = date_create($temp . " " . self::entrada_tarde);
+            $salida_tarde = date_create($temp . " " . self::salida_tarde);
+
+            //Haciendo las pruebas pertinentes:
+            //Si la posible cita es más tarde que las 12:00 y más temprano 
+            //que la 1:00, entonces queda en el almuerzo y no puede ser
+            while($fecha >= $salida_mañana && $fecha < $entrada_tarde) {
+                //QUEDA EN EL ALMUERZO
+                //$isReady = false;
+                $fecha->add(\DateInterval::createFromDateString("30 minute"));
+            }
+
+            //Probando si QUEDA PARA MAÑANA
+            if($fecha >= $salida_tarde) {
+                //Ya no es hora de atender
+                $isReady = false;
+
+                //Avanza un día en la fecha
+                $f_base = date_create(date_format($f_base, ""), $object)
+                $f_base->add(\DateInterval::createFromDateString("1 day"));
+            }
+            
+        } while($isReady == false);
+        
+        return $fecha;
+    }
+    
+    /**
      * Intenta guardar los datos de la instancia de esta clase en la BD como un nuevo registro
      * @return boolean
      */
     public function saveOnDB() {
+        include 'Database.php';
+
         //Creando el objeto de control sobre la base de datos y accediendo a ella
         $database = new Database();
+        
+        //Obteniendo siguiente turno libre
+        $res = $this->getNextTurno();
         
         //Creando una bandera que indique si la receta está lista para ser
         //almacenada en la BD
@@ -133,15 +241,18 @@ class Cita {
         $sentencia = "insert into " . Database::TABLA_CITA
                 . " values (null, "
                 . $this->codigo_medico . ", "
-                . $this->codigo_paciente . ", "
-                . $this->f_cita . ", "
-                . $this->motivo . ", '"
+                . $this->codigo_paciente . ", '"
+                . $this->f_cita . "', '"
+                . $this->motivo . "', '"
                 . $this->diagnostico . "', "
                 . $this->activo . ", "
                 . $this->visible. ")";
         
+        //echo "PASA";
+        //echo $sentencia;
+
         //Si pasa las comprobaciones
-        if($isReady) {
+        if(false) {
             return $database->execute($sentencia);
         }
         else {
